e52af5a5774831e7e733a5fae29943e5d1b22573
Matias Benegas <benegas.matias21@gmail.com>
2026-04-06 13:33:05 -0300

    url api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.prodmanager.com.ar', // Default during migration
+  apiUrl: 'https://prod-manager-api.onrender.com',
   supabaseUrl: 'https://jrcdlzqbadyueodqpsii.supabase.co',
   supabaseAnonKey: 'sb_publishable_Fb1XxcUkSIdPKf1MdWw_BQ_By-uPGdB'
 };
