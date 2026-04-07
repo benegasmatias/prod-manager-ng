3874f88ae01b3484d84d3c652bed6d6faf8a2256
matias <benegas.matias21@gmail.com>
2026-04-06 22:47:09 -0300

    dev prod qa config

---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://qa-manager-api.onrender.com',
+  supabaseUrl: 'https://jrcdlzqbadyueodqpsii.supabase.co',
+  supabaseAnonKey: 'sb_publishable_Fb1XxcUkSIdPKf1MdWw_BQ_By-uPGdB'
+};
