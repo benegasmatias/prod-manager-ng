85be0abf8ddbf9d238b628d0be7c6e97175ab72d
Matias Benegas <benegas.matias21@gmail.com>
2026-04-23 08:56:01 -0300

    terminos y condiciones

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,17 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ConfirmDialogComponent } from '../../libs/shared/ui/confirm-dialog/confirm-dialog.component';
+import { TermsAndConditionsModalComponent } from '../../libs/shared/ui';
 import { ThemeService } from '../../libs/core/layout/theme.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ConfirmDialogComponent],
+  imports: [RouterOutlet, ConfirmDialogComponent, TermsAndConditionsModalComponent],
   template: `
     <router-outlet></router-outlet>
     <app-confirm-dialog></app-confirm-dialog>
+    <app-terms-modal></app-terms-modal>
   `,
   styles: []
 })
